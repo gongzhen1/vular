3a8c889abceb075361874f3b4eab72f74ae07a5c
Water.Li <11011968@qq.com>
2020-01-13 18:54:38 +0800

    Delete vular-menus.php

--- config/vular-menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Water\Vular\Admin\Dashboard\Menu::class,
-    Water\Vular\Webadmin\Media\Menu::class,
-    Water\Vular\Admin\User\Menu::class,
-    Water\Vular\Admin\Settings\Menu::class
-];
